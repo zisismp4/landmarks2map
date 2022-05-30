f735d65b03ccf16f5000b63586120bcd39ae513a
Zisis Batzos <zisisbatzos@outlook.com>
2022-05-30 13:35:33 +0300

    description added

--- dataset_prepare_z.py
+++ dataset_prepare_z.py
@@ -3,7 +3,14 @@ import pandas as pd
 import streamlit as st
 
 def labels_in_map(df):
-    st.title("Labels in map")
+    st.title("Landmarks in map")
+    img_loc_count = 0
+    for id in list(df.dropna()['images']):
+        images = id.split(' ')
+        img_loc_count += len(images)
+
+    st.header('# Landmark IDs with location: ', df.dropna().shape[0])
+    st.header('# Total images with location: ', img_loc_count)
     st.map(df)
 
 root = './files'
@@ -21,14 +28,5 @@ for country in country_json_paths:
     
 df_countries = pd.concat(df_country, ignore_index=True).rename(columns={'id': 'landmark_id'})
 df_final = df_countries.merge(df_merged, on='landmark_id')
-img_count = 0
-img_loc_count = 0
-for id in list(df_final['images']):
-    images = id.split(' ')
-    img_count += len(images)
-
-for id in list(df_final.dropna()['images']):
-    images = id.split(' ')
-    img_loc_count += len(images)
 
 labels_in_map(df_final.dropna())
